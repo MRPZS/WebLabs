316eaadc87459ed647c5afcf35e471c0fd8101a1
MRPZS <161828346+MRPZS@users.noreply.github.com>
2024-03-19 22:35:01 +0300

    Finish task 1

--- lab2/Index.php
+++ lab2/Index.php
@@ -2,10 +2,8 @@
 /* Imagine a lot of code here */ 
  $very_bad_unclear_name = "15 chicken wings";
 
+ $order = & $very_bad_unclear_name; 
+ $order .= " is my dinner";
 
-// Write your code here:
-
-
-  
  // Don't change the line below
  echo "\nYour order is: $very_bad_unclear_name.";
