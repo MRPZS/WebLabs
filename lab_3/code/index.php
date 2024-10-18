6567460fc09d68348db1e2c275a4772c2efdf75b
MRPZS <161828346+MRPZS@users.noreply.github.com>
2024-10-18 12:17:37 +0300

    add input email, options

--- lab_3/code/index.php
+++ lab_3/code/index.php
@@ -5,9 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Avito</title>
 </head>
 <body>
-
+    <form action="save.php" method="post">
+        <input type="email" name="email" required>
+        <select name="category" required>
+            <option>Cars</option>
+            <option>Other</option>
+        </select>
+    </form>
 </body>
 </html>
