5f3b8d1ea7a740a0f2cbb28b22dfb28e8eec2987
MRPZS <161828346+MRPZS@users.noreply.github.com>
2024-10-18 12:54:44 +0300

    add if and comeback again

--- lab_3/code/save.php
+++ lab_3/code/save.php
@@ -8,3 +8,11 @@ $category = $_POST['category'];
 $title = $_POST['title'];
 $email = $_POST['email'];
 $desc = $_POST['description'];
+
+$filePath = "categories/{$category}/{$title}.txt";
+
+if (false === file_put_contents($filePath, "$email\n$desc")) {
+	throw new Exception('Something went wrong.');
+}
+header('Location: /')
+exit()
