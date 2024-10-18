dd2f8be564271d339479bc5c4a09d5a02d9179e3
MRPZS <161828346+MRPZS@users.noreply.github.com>
2024-10-18 12:46:19 +0300

    add data

--- lab_3/code/save.php
+++ lab_3/code/save.php
@@ -3,3 +3,8 @@ if  (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POS
   header('Location: /')
   exit()
 }
+
+$category = $_POST['category'];
+$title = $_POST['title'];
+$email = $_POST['email'];
+$desc = $_POST['description'];
